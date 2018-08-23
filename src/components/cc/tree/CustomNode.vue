f8f36422601460d9f5f62708c63941b4723a2699
c0hb1rd <cohbird.zn@gmail.com>
2018-08-23 18:14:24 +0800

    添加 树 组件

---
+++ src/components/cc/tree/CustomNode.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="custom-content" @click.prevent.stop="clickNode">
+        {{ node.displayName}}
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "",
+        componentName: "",
+        components: {},
+        props: {
+            node: {require: true},
+        },
+        watch: {},
+        methods: {
+            clickNode() {
+                this.$emit("clickNode")
+            }
+        },
+        data() {
+            return {}
+        },
+        created() {
+        },
+        mounted() {
+        },
+        destroyed() {
+        },
+    }
+</script>
+
+<style lang="scss">
+    .custom-content {
+    }
+</style>
---
+++ src/components/cc/tree/TreeNode.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="cc-tree-simple-content flex-row flex-a-center" @click.prevent.stop="nodeClick">
+    {{ node[props.label] }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "CcTreeNode",
+    componentName: "CcTreeNode",
+    props: {
+      node: {require: true},
+      props: {
+        default: () => {
+          return {
+            label: 'label'
+          }
+        }
+      }
+    },
+    methods: {
+      nodeClick() {
+        this.$emit("nodeClick", this.node);
+      }
+    }
+  }
+</script>
